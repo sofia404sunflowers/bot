cb29a2c9aa99a105e48baf45058a281b01991c00
Sofia Mysholovka <sofia_in_sunflowers@Sofias-MacBook-Air.local>
2020-07-06 10:53:39 +0300

    new bot add

---
+++ bot.py
@@ -0,0 +1,17 @@
+import config
+import telebot
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, "Рада тебя видеть! \nКак настроение?")
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
