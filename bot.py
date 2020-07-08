09c79ccb5291b124132db6dfff6bc4f54b7e84dc
Sofia Mysholovka <sofia_in_sunflowers@Sofias-MacBook-Air.local>
2020-07-08 10:54:20 +0300

    work on server

--- bot.py
+++ bot.py
@@ -1,17 +1,13 @@
 import config
 import telebot
-
 bot = telebot.TeleBot(config.TOKEN)
-
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.send_message(message.chat.id, "Рада тебя видеть! \nКак настроение?")
-
+    bot.send_message(message.chat.id,  "Рада тебя видеть! \nКак настроение?")
 
 @bot.message_handler(content_types=['text'])
 def answer(message):
     bot.send_message(message.chat.id, message.text)
 
-
-bot.polling(none_stop=True)
+bot.polling(none_stop = True)
